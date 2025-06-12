73e40b0f98aa95e5c234db51a78be6d70d6f00b5
Ankul Yadav <ankul.yadav@aayaninfotech.com>
2025-06-12 17:09:53 +0530

    fix this error

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -5,14 +5,14 @@ function asyncHandler(fn) {
         try {
             await fn(req, res, next)
         } catch (error) {
-            if (req.files) {
-                const keyNames = Object.keys(req.files)[0];
-                req.files[keyNames].forEach(file => {
-                    if (file.path) {
-                        fs.unlinkSync(file.path);
-                    }
-                });
-            }
+            // if (req.files) {
+            //     const keyNames = Object.keys(req.files)[0];
+            //     req.files[keyNames].forEach(file => {
+            //         if (file.path) {
+            //             fs.unlinkSync(file.path);
+            //         }
+            //     });
+            // }
             res.status(error.statusCode || 500).json({
                 statusCode: error.statusCode || 500,
                 data: null,
